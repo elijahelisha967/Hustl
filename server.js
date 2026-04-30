c287e035e7ef2bbcd6581c796d66262c7ef7bfb6
elijahelisha967 <elishaelijah878@gmail.com>
2026-04-30 07:53:32 +0100

    fix port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const bcrypt = require('bcryptjs');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
